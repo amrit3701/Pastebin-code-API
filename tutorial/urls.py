161a03b1fcd10e0f09e4907f08535680725a815a
Amritpal Singh <amrit3701@gmail.com>
2017-12-08 19:10:02 +0530

    Tutorial 4: Authentication & Permissions

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -20,4 +20,6 @@ from django.conf.urls import url, include
 urlpatterns = [
 #    path('admin/', admin.site.urls),
     url(r'^', include('snippets.urls')),
+    url(r'^api-auth/', include('rest_framework.urls',
+                               namespace='rest_framework')),
 ]
